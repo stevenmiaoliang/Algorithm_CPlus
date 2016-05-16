d4d8a97bca83fcddc71cc676c0faf30205faa38b
miaoliang <miaoliang@ubuntu>
2016-05-15 18:39:02 -0700

    First Create Project

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=algorithm_cplus
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/algorithm_cplus
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=algorithmcplus.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/algorithmcplus.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=algorithm_cplus
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/algorithm_cplus
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=algorithmcplus.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/algorithmcplus.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
